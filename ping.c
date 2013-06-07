338d1b60fd5a3e28f40ed4f08264df522e1719ba
june <abgood@sina.com>
2013-06-07 09:19:06 +0800

    del all and restart

--- ping.c
+++
@@ -1,6 +0,0 @@
-#include "check.h"
-
-/* ping检查 */
-void check_ping(site_info info) {
-    printf("%d\n", info->site_id);
-}
--- ping.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHECK_PING_H
-#define CHECK_PING_H
-
-#include "info.h"
-
-/* ping检查 */
-void check_ping(site_info);
-
-#endif
